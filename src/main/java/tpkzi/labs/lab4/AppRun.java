6d1e2006d73bd237b942d48c125a1a764d45b6a9
Tanya <t.v.gayuk@gmail.com>
2016-03-07 00:59:09 +0200

    Run AppRun class

---
+++ src/main/java/tpkzi/labs/lab4/AppRun.java
@@ -0,0 +1,65 @@
+package tpkzi.labs.lab4;/**
+ * Created by Таня on 06/03/2016.
+ */
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import tpkzi.labs.lab4.coder.CyclicCoder;
+
+import javax.swing.*;
+
+public class AppRun extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Cyclic coder");
+        final GridPane pane = new GridPane();
+        pane.setAlignment(Pos.CENTER);
+        pane.setHgap(10);
+        pane.setVgap(10);
+        pane.setPadding(new Insets(25));
+        Scene scene = new Scene(pane, 300, 300);
+        pane.add(new Label("Input bits"), 0, 0);
+        TextField inData = new TextField();
+        inData.setDisable(true);
+        pane.add(inData, 0, 1);
+        Button encodeDecode = new Button("Encode - decode!");
+        pane.add(encodeDecode, 0, 2);
+        pane.add(new Label("Decoding result"), 0, 4, 2, 1);
+        TextArea outData = new TextArea();
+        pane.add(outData, 0, 6);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        encodeDecode.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                int d = 0b1101;
+                //int d = Integer.parseInt(inData.getText());
+                int p = 0b1011;
+                inData.setText(Integer.toBinaryString(d));
+                CyclicCoder coder = new CyclicCoder();
+                int x = coder.encode(d);
+                //System.out.println(Integer.toBinaryString(x));
+                outData.appendText(Integer.toBinaryString(x) + "\n");
+                //System.out.println(Integer.toBinaryString(coder.decode(0b1100001)));
+                outData.appendText(Integer.toBinaryString(coder.decode(0b1100001)));
+            }
+        });
+
+    }
+}
